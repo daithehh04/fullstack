6d9ab39da73184df02632129c8f024ce5a0b5693
daithehh04 <daithehh04@gmail.com>
2023-09-29 23:08:31 +0700

    day32

---
+++ day32/assets/js/component.js
@@ -0,0 +1,62 @@
+class F8 {
+  static component(name, option) {
+    customElements.define(
+      name,
+      class extends HTMLElement {
+        constructor() {
+          super();
+        }
+        connectedCallback() {
+          if (option.data) {
+            const data = option.data();
+            Object.keys(data).forEach((key) => {
+              window[key] = data[key];
+            });
+          }
+          if (option.template) {
+            const template = option.template;
+            const regex = template.match(/{{.+?}}/g);
+            const regexArr = [];
+            regex?.forEach((value) => {
+              const variableResult = value.match(/{{(.+?)}}/);
+              regexArr.push(variableResult[1].trim());
+            });
+            var replaceTemplate = template;
+            regexArr.forEach((key) => {
+              replaceTemplate = replaceTemplate.replace(
+                /{{.+?}}/,
+                `${window[key]}`
+              );
+            });
+
+            const templateEle = document.createElement("template");
+            templateEle.innerHTML = replaceTemplate;
+            const templateNode = templateEle.content.cloneNode(true);
+            const btnCounts = templateNode.querySelectorAll("button");
+            const countNumber = templateNode.querySelector(".count");
+            const h1 = templateNode.querySelector("h1");
+            btnCounts.forEach((btn) => {
+              const nameAttribute = btn.getAttributeNames();
+              const nameEvent = nameAttribute[0].split("v-on:");
+              const eventBtn = nameEvent[1];
+              const btnAttribute = btn.getAttribute(`v-on:${eventBtn}`);
+              btn.addEventListener(eventBtn, () => {
+                if (btnAttribute === "count--") {
+                  countNumber.innerText = --count;
+                }
+                if (btnAttribute === "count++") {
+                  countNumber.innerText = ++count;
+                }
+                if (btnAttribute.includes("title=")) {
+                  let contentTitle = btnAttribute.split("title=");
+                  h1.innerText = contentTitle[1];
+                }
+              });
+            });
+            this.append(templateNode);
+          }
+        }
+      }
+    );
+  }
+}
---
+++ day32/assets/js/main.js
@@ -0,0 +1,18 @@
+F8.component("counter-app", {
+  data: () => ({
+      count: 0,
+      title: "Counter App",
+  }),
+  template: `
+      <h1>{{title}}</h1>
+      <h2>Count: <span class="count">{{ count }}</span></h2>
+      <button v-on:click="count--">-</button>
+      <button v-on:click="count++">+</button>
+      <button v-on:dblclick="title=Xin chào F8">Change Title</button>
+  `,
+});
+F8.component("header-component", {
+  template: `
+      <h1>HEADER</h1>
+  `,
+});
