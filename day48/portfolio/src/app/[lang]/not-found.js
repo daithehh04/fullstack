a24edded78c57221ae2312b1b354b6f0593c2498
daithehh04 <daithehh04@gmail.com>
2023-11-30 10:05:19 +0700

    add not found

--- day48/portfolio/src/app/[lang]/not-found.js
+++ day48/portfolio/src/app/[lang]/not-found.js
@@ -1,6 +1,14 @@
+import { Button } from "@nextui-org/react"
+import Link from "next/link"
+
 function NotFound() {
   return (
-    <div>Page not found</div>
+    <div className="h-[100vh] w-full flex flex-col items-center justify-center">
+      <div className="text-[4rem] font-semibold">Page Not Found !!!</div>
+      <Button radius="sm" color="danger">
+        <Link href={'/'} className="text-[1rem] font-medium">Go back</Link>
+      </Button>
+    </div>
   )
 }
 
