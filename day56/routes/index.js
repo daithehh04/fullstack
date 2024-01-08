c22e61d0d0440443b8abf184c9bfb54121d35900
daithehh04 <daithehh04@gmail.com>
2024-01-08 22:11:04 +0700

    add auth

--- day56/routes/index.js
+++ day56/routes/index.js
@@ -1,15 +1,16 @@
 var express = require("express");
 var router = express.Router();
 const homeController = require("../controllers/home.controller");
+const authController = require("../controllers/auth.controller");
 const validate = require("../utils/validate");
 
 /* GET home page. */
 router.get("/", homeController.index);
-router.get("/login", homeController.login);
-router.post("/login", validate.login(), homeController.handleLogin);
+router.get("/login", authController.login);
+router.post("/login", validate.login(), authController.handleLogin);
 
-router.get("/register", homeController.register);
-router.post("/register", validate.register(), homeController.handleRegister);
-router.get("/logout", homeController.logout);
+router.get("/register", authController.register);
+router.post("/register", validate.register(), authController.handleRegister);
+router.get("/logout", authController.logout);
 
 module.exports = router;
