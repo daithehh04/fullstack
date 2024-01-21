fafd7a596f82fbb9476008721b75d16f0e7bea1b
daithehh04 <daithehh04@gmail.com>
2024-01-21 16:15:18 +0700

    day 60

--- day60/config/config.js
+++ day60/config/config.js
@@ -1,27 +1,46 @@
 require("dotenv").config()
+const pg = require("pg")
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    define: {
-      underscored: true,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_DIALECT || "postgres",
+    port: process.env.POSTGRES_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
     },
+    dialectModule: pg,
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_DIALECT || "postgres",
+    port: process.env.POSTGRES_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+    dialectModule: pg,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_DIALECT || "postgres",
+    port: process.env.POSTGRES_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+    dialectModule: pg,
   },
 }
